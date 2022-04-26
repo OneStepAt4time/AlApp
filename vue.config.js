d4ee61fa1d81601ecd48273125c1c812ff640f7f
Emanuele SANTONASTASO <emanuele.santonastaso@studenti.unipr.it>
2022-04-26 18:23:12 +0200

    ADD: csv loaders and other modules

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  chainWebpack: config => {
+    config
+      .module
+      .rule('csv')
+      .test(/\.csv$/)
+      .use('csv-loader')
+      .loader('csv-loader')
+      .options({
+        dynamicTyping: true,
+        header: true,
+        skipEmptyLines: true
+      })
+      .end()
+  }
+}
